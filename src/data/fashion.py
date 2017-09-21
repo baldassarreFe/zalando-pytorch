13ad91d368edd02badb3ca203589a1f7ddf0921f
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-09-21 22:15:30 +0200

    Subclassed MNIST as FashionMNIST (copied from a pytorch commit)
    Actually copied from https://github.com/pytorch/vision/commit/eec5ba4405c8815bd1797619d9cc9276f81b76f4

---
+++ src/data/fashion.py
@@ -0,0 +1,27 @@
+from torchvision.datasets import MNIST
+
+class FashionMNIST(MNIST):
+    """`Fashion MNIST <https://github.com/zalandoresearch/fashion-mnist>`_ Dataset.
+    """
+    urls = [
+        'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz',
+        'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz',
+        'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz',
+        'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz',
+    ]
+
+    input_shape = (28, 28)
+    num_classes = 10
+
+    labels = [
+        'T-shirt/top',
+        'Trouser',
+        'Pullover',
+        'Dress',
+        'Coat',
+        'Sandal',
+        'Shirt',
+        'Sneaker',
+        'Bag',
+        'Ankle boot'
+    ]
